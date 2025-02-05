73d0455e76cf16a432b58f6c114779b5df5f5b62
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-06 00:46:02 +0330

    style: refine NavbarLayout with improved spacing and updated version display

--- src/layouts/navbar/navbar.layout.tsx
+++ src/layouts/navbar/navbar.layout.tsx
@@ -8,9 +8,11 @@ export interface PageLink {
 export function NavbarLayout(): JSX.Element {
 	return (
 		<nav className="flex items-center justify-between px-4 py-2 bg-[#212121] shadow mb-5">
-			<div className="flex">
-				<h1 className="text-2xl font-bold text-blue-400 font-[balooTamma]">Widgetify</h1>
-				<p className="text-xs text-blue-100 font-[balooTamma]">v beta</p>
+			<div className="flex items-center gap-1">
+				<h1 className="text-2xl font-bold text-blue-400 font-[balooTamma] tracking-wide">
+					Widgetify
+				</h1>
+				<p className="text-xs text-blue-200 font-[balooTamma] mt-1">v0</p>
 			</div>
 			<div>
 				<a
