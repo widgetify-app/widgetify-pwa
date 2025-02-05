137c2ddfdb603a5d8094bd17798a074eba06720d
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-06 00:22:54 +0330

    feat: update NavbarLayout with GitHub link and version display

--- src/layouts/navbar/navbar.layout.tsx
+++ src/layouts/navbar/navbar.layout.tsx
@@ -1,3 +1,5 @@
+import { FaGithub } from 'react-icons/fa6'
+
 export interface PageLink {
 	name: string
 	to: string
@@ -6,8 +8,20 @@ export interface PageLink {
 export function NavbarLayout(): JSX.Element {
 	return (
 		<nav className="flex items-center justify-between px-4 py-2 bg-[#212121] shadow mb-5">
-			<div className="flex items-center">
-				<h1 className="text-2xl font-bold text-gray-300">Widgetify 🪙</h1>
+			<div className="flex">
+				<h1 className="text-2xl font-bold text-blue-400 font-[balooTamma]">Widgetify</h1>
+				<p className="text-xs text-blue-100 font-[balooTamma]">v beta</p>
+			</div>
+			<div>
+				<a
+					href="https://github.com/widgetify-app/widgetify-pwa"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<div className="hover:bg-[#333] p-2 rounded-md cursor-pointer">
+						<FaGithub className="text-2xl text-gray-500" />
+					</div>
+				</a>
 			</div>
 		</nav>
 	)
