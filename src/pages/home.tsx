61bbf4a37a6410a22b737cde926ddfc96c881b03
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-06 15:49:05 +0330

    style: update HomePage header with new title and icon

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
+import { BiBroadcast } from 'react-icons/bi'
 import { IoPulse } from 'react-icons/io5'
+import { MdTrendingUp } from 'react-icons/md'
 import { StoreKey } from '../common/constant/store.key'
 import { getFromStorage, setToStorage } from '../common/storage'
 import { AddCurrencyBox } from '../components/AddCurrencyBox'
@@ -44,8 +46,8 @@ export function HomePage() {
 			}}
 		>
 			<section className="p-2 mx-1 rounded shadow lg:mx-4 bg-neutral-100 dark:bg-neutral-800">
-				<h2 className="flex items-center gap-1 mb-4 text-lg font-semibold dark:text-gray-200">
-					<IoPulse /> Currency Rates
+				<h2 className="flex  items-center text-center gap-1 mb-4 text-lg font-semibold dark:text-gray-200 font-[balooTamma]">
+					🪙 ArzLive
 				</h2>
 				<div className="grid grid-cols-2 gap-2 p-1 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6">
 					{selectedCurrencies.map((currency, index) => (
