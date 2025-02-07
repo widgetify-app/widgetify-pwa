79684ebe9b4fbe66eb654fcf318b81458ec60825
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-07 15:46:06 +0330

    feat: add long press functionality to open currency modal in CurrencyBox component

--- src/components/currency-box.tsx
+++ src/components/currency-box.tsx
@@ -6,6 +6,8 @@ import { getMainColorFromImage } from '../common/color'
 import type { Currency } from '../common/interface/currency.interface'
 import { getFromStorage, setToStorage } from '../common/storage'
 import { useGetCurrencyByCode } from '../services/getMethodHooks/getCurrencyByCode.hook'
+import Modal from './modal'
+
 interface CurrencyBoxProps {
 	code: string
 }
@@ -19,6 +21,7 @@ export const CurrencyBox = ({ code }: CurrencyBoxProps) => {
 	const [imgColor, setImgColor] = useState<string>()
 	const [displayPrice, setDisplayPrice] = useState(0)
 	const [priceChange, setPriceChange] = useState(0)
+	const [isModalOpen, setIsModalOpen] = useState(false)
 
 	const prevPriceRef = useRef<number | null>(null)
 
@@ -79,8 +82,40 @@ export const CurrencyBox = ({ code }: CurrencyBoxProps) => {
 		return () => unsubscribe()
 	}, [animatedPrice, currency?.rialPrice])
 
+	function toggleCurrencyModal() {
+		if (!isModalOpen === true) {
+			// vibration
+			if ('vibrate' in navigator) {
+				navigator.vibrate(100)
+			}
+		}
+		setIsModalOpen(!isModalOpen)
+	}
+
+	const longPressTimeout = useRef<NodeJS.Timeout | null>(null)
+
+	const handleMouseDown = () => {
+		longPressTimeout.current = setTimeout(() => {
+			toggleCurrencyModal()
+		}, 500) // 500ms for long press
+	}
+
+	const handleMouseUp = () => {
+		if (longPressTimeout.current) {
+			clearTimeout(longPressTimeout.current)
+			longPressTimeout.current = null
+		}
+	}
+
 	return (
-		<div className="flex flex-col items-center justify-between h-24 p-2 rounded-lg shadow-sm sm:w-32 dark:bg-neutral-700 w-36">
+		<div
+			className="flex flex-col items-center justify-between h-24 p-2 rounded-lg shadow-sm sm:w-32 dark:bg-neutral-700 w-36"
+			onClick={() => toggleCurrencyModal()}
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}
+			onTouchStart={handleMouseDown}
+			onTouchEnd={handleMouseUp}
+		>
 			<div className="relative flex flex-row items-center justify-between w-full">
 				<>
 					<img
@@ -89,10 +124,8 @@ export const CurrencyBox = ({ code }: CurrencyBoxProps) => {
 						className="object-cover w-4 h-4 rounded-full "
 					/>
 					<div
-						className={'w-5 h-5 absolute rounded-full z-0 left-1.5 blur-lg'}
-						style={{
-							backgroundImage: `radial-gradient(50% 50% at 50% 50%, ${imgColor} 55%, ${`${imgColor}00`} 100%)`,
-						}}
+						className="absolute z-10 w-5 h-5 opacity-50 top-1 blur-md"
+						style={{ backgroundColor: imgColor }}
 					></div>
 				</>
 				<div className="flex flex-col w-full px-2">
@@ -120,6 +153,40 @@ export const CurrencyBox = ({ code }: CurrencyBoxProps) => {
 					) : null}
 				</motion.p>
 			</div>
+
+			<Modal
+				isOpen={isModalOpen}
+				onClose={() => toggleCurrencyModal()}
+				size="sm"
+				title=""
+			>
+				<div className="flex flex-col items-center justify-center space-y-6 font-[balooTamma] p-4">
+					<div className="relative">
+						<img
+							src={currency?.icon}
+							alt={currency?.name?.en}
+							className="z-50 object-cover w-16 h-16 rounded-full shadow"
+						/>
+						<div
+							className="absolute top-0 z-10 w-16 h-16 blur-xl opacity-30"
+							style={{ backgroundColor: imgColor }}
+						/>
+					</div>
+					<div className="text-center">
+						<p className="text-xl font-bold text-gray-800 dark:text-gray-200">
+							{currency?.name.en}
+						</p>
+						<p className="text-sm font-medium text-gray-500 dark:text-gray-400">
+							{code.toUpperCase()}
+						</p>
+					</div>
+					<div className="space-y-2">
+						<div className="relative text-xl font-bold text-gray-900 dark:text-gray-200">
+							<p>{displayPrice !== 0 ? displayPrice.toLocaleString() : ''} </p>
+						</div>
+					</div>
+				</div>
+			</Modal>
 		</div>
 	)
 }
