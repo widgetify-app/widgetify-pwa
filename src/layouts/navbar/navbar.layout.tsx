c73db19272ed38fe39dbcc3b128601374c968f60
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-20 15:46:30 +0330

    style(navbar): update background color for GitHub icon in dark mode

--- src/layouts/navbar/navbar.layout.tsx
+++ src/layouts/navbar/navbar.layout.tsx
@@ -16,7 +16,7 @@ export function NavbarLayout(): JSX.Element {
 				href="https://github.com/widgetify-app/widgetify-pwa"
 				target="_blank"
 				rel="noopener noreferrer"
-				className="flex items-center justify-center w-10 h-10 bg-[#1e1e1e] rounded-xl text-gray-400 hover:text-gray-300 hover:bg-[#252525] transition-colors"
+				className="flex items-center justify-center w-10 h-10 dark:bg-[#1e1e1e] rounded-xl text-gray-400 hover:text-gray-300 hover:bg-[#252525] transition-colors"
 			>
 				<FaGithub size={22} />
 			</a>
